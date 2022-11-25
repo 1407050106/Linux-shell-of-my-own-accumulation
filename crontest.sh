896880e2cd4aad0692fe43decf135c4d1c3b10fd
YonglinWang <41557033+1407050106@users.noreply.github.com>
2022-11-25 09:33:45 +0800

    Update crontest.sh

--- crontest.sh
+++ crontest.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #cd /etc/cron.d 
 #crontab crontest.cron 
+# is_exit=`ps w | grep DAQ.exe | grep -v grep | wc -l `
 mkdir -p /var/spool/cron/crontabs && touch /var/spool/cron/crontabs/root && echo "* */1 * * * ntpdate 10.64.4.154" > /var/spool/cron/crontabs/root && crond &
