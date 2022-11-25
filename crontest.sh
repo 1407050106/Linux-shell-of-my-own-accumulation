ab74c3663577319f43ee0f519581302eeebccd40
YonglinWang <41557033+1407050106@users.noreply.github.com>
2022-11-25 09:30:16 +0800

    Update crontest.sh

--- crontest.sh
+++ crontest.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 #cd /etc/cron.d 
 #crontab crontest.cron 
-mkdir /var/spool/cron/crontabs && crontab -e
-*/1 * * * * echo"FUCK" > /etc/cron.d/expr
-:wq
-crontab crontest.cron 
+mkdir -p /var/spool/cron/crontabs && touch /var/spool/cron/crontabs/root && echo "* */1 * * * ntpdate 10.64.4.154" > /var/spool/cron/crontabs/root && crond &
